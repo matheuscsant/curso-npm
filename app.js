71a7c534df2fa0de86dcad836549b2f3f8ae791a
Matheus Campanhola <matheuscampanholasantos123@gmail.com>
2024-05-19 09:57:54 -0300

    Endpoint com parâmetro

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-import tabela2024 from "./tabela.js";
 import express from "express";
+import tabela2024 from "./tabela.js";
 
 const app = express();
 
-app.get("/getTimes", (req, res) => {
-  console.log(req.url);
-  console.log(req.statusCode);
+app.get("/", (req, res) => {
   res.send(tabela2024);
+});
+
+app.get("/:sigla", (req, res) => {
+  let param = req.params.sigla;
+  let timeSelecionado = tabela2024.filter((time) => time.sigla == param);
+  if (timeSelecionado.length == 0) {
+    console.log("NENHUM TIME ENCONTRADO");
+    res.send("NENHUM TIME ENCONTRADO");
+  } else {
+    console.log("TIME ENCONTRADO: " + timeSelecionado.sigla);
+    res.send(timeSelecionado);
+  }
 });
 
 app.listen(300, () => console.log("Servidor rodando com sucesso"));
